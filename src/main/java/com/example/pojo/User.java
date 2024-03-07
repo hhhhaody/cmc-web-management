05554b25950e9604e6c7e1fe5658ceef24215832
zhangonfire <zhang_on_fire@proton.me>
2024-03-07 09:39:52 +0800

    用户管理功能

--- src/main/java/com/example/pojo/User.java
+++ src/main/java/com/example/pojo/User.java
@@ -1,30 +1,31 @@
 package com.example.pojo;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/**
+ *用户实体类
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    private Long id;
     private String username;
+    private String name;
     private String password;
-
-    public User() {
-    }
-
-    public User(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    private String phone;
+    private String sex;
+    private String idNumber;//身份证号码 驼峰命名法 ---> 映射的字段名为 id_number
+    private Integer status;
+    private String adminType;
+    private LocalDateTime createTime;
+    private LocalDateTime updateTime;
+    private Long createUser;
+    private Long updateUser;
 }
