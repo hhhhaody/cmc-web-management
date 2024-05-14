cc0b93b70bc7b6a65eca02dde2eecc4d64ae703b
hhhhaody <deyanh@bu.edu>
2024-05-14 17:22:58 +0800

    数据联调

--- src/main/java/com/example/service/impl/DataServiceImpl.java
+++ src/main/java/com/example/service/impl/DataServiceImpl.java
@@ -36,11 +36,11 @@ public class DataServiceImpl implements DataService {
     @Transactional
     @Override
     public void alarm(AlarmData alarmData) {
-
-        //根据设备反馈数据更新报警表alarm_mapping
-        dataMapper.setAlarm(alarmData.getAlarmID(), alarmData.getState());
-
-
+        try {
+            //根据设备反馈数据更新报警表alarm_mapping
+            dataMapper.setAlarm(alarmData.getAlarmID(), alarmData.getState());
+        } catch (Exception e) {
+        }
         dataMapper.alarm(alarmData);
     }
 
@@ -48,17 +48,20 @@ public class DataServiceImpl implements DataService {
     @Override
     public void material(MaterialData materialData) {
         //更新物料库存表
-        Material material = materialMapper.getById(Math.toIntExact(materialData.getMaterialID()));
+        try {
+            Material material = materialMapper.getById(Math.toIntExact(materialData.getMaterialID()));
 
-        if (material.getUsage() == null){
-            materialMapper.setUsage(material.getId(),"usage",materialData.getAmount());
+            if (material.getUsage() == null) {
+                materialMapper.setUsage(material.getId(), "usage", materialData.getAmount());
+            } else {
+                materialMapper.setUsage(material.getId(), "usage", material.getUsage() + materialData.getAmount());
+            }
+            String sectionName = dataMapper.getSectionName(materialData.getSectionID());
+            Integer section_usage = materialMapper.getUsage(material.getId(), sectionName);
+            materialMapper.setUsage(material.getId(), sectionName, section_usage + materialData.getAmount());
+        } catch (Exception e) {
+
         }
-        else{
-            materialMapper.setUsage(material.getId(),"usage",material.getUsage()+materialData.getAmount());
-        }
-        String sectionName = dataMapper.getSectionName(materialData.getSectionID());
-        Integer section_usage = materialMapper.getUsage(material.getId(),sectionName);
-        materialMapper.setUsage(material.getId(), sectionName, section_usage+materialData.getAmount());
         dataMapper.material(materialData);
     }
 
@@ -66,89 +69,95 @@ public class DataServiceImpl implements DataService {
     @Override
     public void product(ProductData productData) {
         //更新产品表生产量
-        String sectionName = dataMapper.getSectionName(productData.getSectionID());
-        Integer section_production = productMapper.getProductionAmount(productData.getProductID(),sectionName);
-        productMapper.setProductionAmount(productData.getProductID(), sectionName, section_production+productData.getAmount());
+        try {
+            String sectionName = dataMapper.getSectionName(productData.getSectionID());
+            Integer section_production = productMapper.getProductionAmount(productData.getProductID(), sectionName);
+            productMapper.setProductionAmount(productData.getProductID(), sectionName, section_production + productData.getAmount());
+        } catch (Exception e) {
 
+        }
         dataMapper.product(productData);
     }
 
     @Override
     @Transactional
     public void state(StateData stateData) {
-        //根据设备反馈数据更新设备映射表device_mapping
-        String state = dataMapper.getStateInfo(stateData.getStateID());
-        dataMapper.setState(stateData.getStationID(),state);
+        try {
+            //根据设备反馈数据更新设备映射表device_mapping
+            String state = dataMapper.getStateInfo(stateData.getStateID());
+            dataMapper.setState(stateData.getStationID(), state);
 
-        //处理报警与检修维护逻辑判断
-        String status = dataMapper.getStatusInfo(stateData.getStationID());
-        String statusNew;
+            //处理报警与检修维护逻辑判断
+            String status = dataMapper.getStatusInfo(stateData.getStationID());
+            String statusNew;
 
-        if(status.equals("报警")){
+            if (status.equals("报警")) {
 //            dataMapper.setStatus(stateData.getStationID(),"报警");
-        } else if (status.equals("检修维护")) {
+            } else if (status.equals("检修维护")) {
 //            dataMapper.setStatus(stateData.getStationID(),"检修维护");
-        } else{
-            switch(state) {
-                case "手动运行":
-                case "自动运行":
-                case "暂停":
-                    statusNew = "正常运行";
-                    break;
-                case "报警":
-                    statusNew = "报警";
+            } else {
+                switch (state) {
+                    case "手动运行":
+                    case "自动运行":
+                    case "暂停":
+                        statusNew = "正常运行";
+                        break;
+                    case "报警":
+                        statusNew = "报警";
 
-                    //推送报警信息到信息中心表
-                    Facility facility = facilityMapper.getByMappingId(stateData.getStationID());
-                    dataMapper.info("设备:【"+facility.getSection()+"-"+facility.getStation()+"-" +facility.getName()+"】故障报警");
-                    break;
-                case "停止":
-                    statusNew = "停机";
-                    break;
-                default:
-                    statusNew = "检修维护";
+                        //推送报警信息到信息中心表
+                        Facility facility = facilityMapper.getByMappingId(stateData.getStationID());
+                        dataMapper.info("设备:【" + facility.getSection() + "-" + facility.getStation() + "-" + facility.getName() + "】故障报警");
+                        break;
+                    case "停止":
+                        statusNew = "停机";
+                        break;
+                    default:
+                        statusNew = "检修维护";
+                }
+                dataMapper.setStatus(stateData.getStationID(), statusNew);
+
             }
-            dataMapper.setStatus(stateData.getStationID(), statusNew);
 
+
+        } catch (Exception e) {
         }
-
-
-
         dataMapper.state(stateData);
     }
 
     @Override
     @Transactional
     public void timeConsumed(TimeConsumedData timeConsumedData) {
-        LocalDate now = LocalDate.now();
-        timeConsumedData.setTimeConsumed(timeConsumedData.getTimeConsumed()/1000);
-        Product product = productMapper.getById(Math.toIntExact(timeConsumedData.getProductID()));
-        Facility facility = facilityMapper.getByMappingId(timeConsumedData.getStationID());
-        String batchStr = PinYinUtil.getBatchStringDate(product.getName(), String.valueOf(product.getBatch()), now.atStartOfDay(), Math.toIntExact(timeConsumedData.getCurrentNo()));
+        try {
+            LocalDate now = LocalDate.now();
+            timeConsumedData.setTimeConsumed(timeConsumedData.getTimeConsumed() / 1000);
+            Product product = productMapper.getById(Math.toIntExact(timeConsumedData.getProductID()));
+            Facility facility = facilityMapper.getByMappingId(timeConsumedData.getStationID());
+            String batchStr = PinYinUtil.getBatchStringDate(product.getName(), String.valueOf(product.getBatch()), now.atStartOfDay(), Math.toIntExact(timeConsumedData.getCurrentNo()));
 
-        Integer item_id = dataMapper.getItemIdByItemCode(batchStr);
+            Integer item_id = dataMapper.getItemIdByItemCode(batchStr);
 
-        //新增production_item
-        if(item_id == null ){
-            dataMapper.insertProductionItem(batchStr,product.getName(),product.getSpec(),now);
+            //新增production_item
+            if (item_id == null) {
+                dataMapper.insertProductionItem(batchStr, product.getName(), product.getSpec(), now);
 
-            item_id = dataMapper.getItemIdByItemCode(batchStr);
+                item_id = dataMapper.getItemIdByItemCode(batchStr);
+            }
+
+            //新增production_time
+            //查找设备对应工位
+            Integer production_line_id = productionLineMapper.findBySectionAndStation(facility.getSection(), facility.getStation()).getId();
+
+            //如果已存在同工位耗时，说明此工位包含多种设备，将耗时累加
+            Long time = dataMapper.getTimeSpent(item_id, production_line_id);
+            if (time != null) {
+                dataMapper.setTimeSpent(item_id, production_line_id, time + timeConsumedData.getTimeConsumed());
+            } else {
+                //不存在则新增耗时
+                dataMapper.postTimeSpent(item_id, production_line_id, timeConsumedData.getTimeConsumed());
+            }
+        } catch (Exception e) {
         }
-
-        //新增production_time
-        //查找设备对应工位
-        Integer production_line_id = productionLineMapper.findBySectionAndStation(facility.getSection(),facility.getStation()).getId();
-
-        //如果已存在同工位耗时，说明此工位包含多种设备，将耗时累加
-        Long time = dataMapper.getTimeSpent(item_id,production_line_id);
-        if(time != null){
-            dataMapper.setTimeSpent(item_id,production_line_id,time+timeConsumedData.getTimeConsumed());
-        }
-        else{
-            //不存在则新增耗时
-            dataMapper.postTimeSpent(item_id,production_line_id,timeConsumedData.getTimeConsumed());
-        }
-
         dataMapper.timeConsumed(timeConsumedData);
     }
 
@@ -164,12 +173,13 @@ public class DataServiceImpl implements DataService {
 
     /**
      * 设备状态由报警改为检修维护
+     *
      * @param id
      */
     @Override
     public void setStatus(Long id) {
         //更改设备状态为检修维护
-        dataMapper.setStatus(id,"检修维护");
+        dataMapper.setStatus(id, "检修维护");
 
     }
 
@@ -192,7 +202,7 @@ public class DataServiceImpl implements DataService {
     @Override
     @Transactional
     public PageBean listEnergyDates(Integer page, Integer pageSize, String section, LocalDateTime dateStart, LocalDateTime dateEnd) {
-        PageHelper.startPage(page,pageSize);
+        PageHelper.startPage(page, pageSize);
 
         List<EnergyRecord> energyRecords = dataMapper.listEnergyDates(section, dateStart, dateEnd);
         Page<EnergyRecord> p = (Page<EnergyRecord>) energyRecords;
@@ -204,7 +214,7 @@ public class DataServiceImpl implements DataService {
     @Override
     public List<ProductionDetailDto> getTimeConsumed(String section) {
         ProductionDetailDto productionDetailDto = dataMapper.getTimeConsumed(section);
-        Integer productId = productMapper.getId(productionDetailDto.getItemName(),productionDetailDto.getItemModel());
+        Integer productId = productMapper.getId(productionDetailDto.getItemName(), productionDetailDto.getItemModel());
         ProductionDetailDto theoreticalTime = dataMapper.getTheoreticalTime(productId);
         List<ProductionDetailDto> resultList = Arrays.asList(productionDetailDto, theoreticalTime);
         return resultList;
